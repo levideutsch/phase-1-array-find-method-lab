7af3f08cf34a94b86fd7a4c17b44d86422c2ff13
levideutsch <levideutsch@live.com>
2022-09-16 18:05:20 -0400

    commit tree 27b4e297c1f6c975f5332fd8e3f0ba99f8726370

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+  function superbowlWin(arrayOfObjects) {
+    //  const found = arrayOfObjects.find(findWin) 
+    //  function findWin(object) {
+    //     if (object.result === "W") {
+    //         return object
+    //     }
+
+    //  }
+     const found2 = arrayOfObjects.find(object => object.result === "W") 
+     console.log(found2)
+    // return (!!found ? found.year : undefined)
+    if (found2)  {
+       return found2.year
+    } else {
+        return undefined
+    }
+}
+
+  
+// function isWin(game) {
+//   return game.result === "W"
+    
